66b8292f8975d1c5a42870ea0cde0df4efc65516
Dzjanis <roah.thaar.g@gmail.com>
2025-08-18 22:06:50 +0300

    inProgress

--- src/components/headerComponents/NavigationMenu.tsx
+++ src/components/headerComponents/NavigationMenu.tsx
@@ -1,19 +1,25 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export const NavigationMenu = () => {
   return (
-    <nav className='flex justify-center items-center gap-4 w-auto'>
-      <NavLink to='/' className={({ isActive }) => (isActive ? 'text-blue-500 font-bold' : 'text-gray-700')}>
-        FrontPage
-      </NavLink>
+    <div className='flex justify-between items-center w-2/3'>
+      <Link to='/'>
+        <img src='../../asets/banquee.png' alt='' />
+      </Link>
+      <nav className='flex justify-center items-center gap-4 w-auto font-custom'>
+        <NavLink to='/' className={({ isActive }) => (isActive ? 'text-blue-500 font-bold' : 'text-gray-700')}>
+          FrontPage
+        </NavLink>
 
-      <NavLink to='/features' className={({ isActive }) => (isActive ? 'text-blue-500 font-bold' : 'text-gray-700')}>
-        Features
-      </NavLink>
+        <NavLink to='/features' className={({ isActive }) => (isActive ? 'text-blue-500 font-bold' : 'text-gray-700')}>
+          Features
+        </NavLink>
 
-      <NavLink to='/compare' className={({ isActive }) => (isActive ? 'text-blue-500 font-bold' : 'text-gray-700')}>
-        Compare
-      </NavLink>
-    </nav>
+        <NavLink to='/compare' className={({ isActive }) => (isActive ? 'text-blue-500 font-bold' : 'text-gray-700')}>
+          Compare
+        </NavLink>
+      </nav>
+      <button>ThemeToggle</button>
+    </div>
   );
 };
