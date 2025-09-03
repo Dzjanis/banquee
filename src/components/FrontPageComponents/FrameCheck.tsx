86cc077a8ef200d3762ddfdd021603333440de1e
Dzjanis <roah.thaar.g@gmail.com>
2025-09-03 19:32:10 +0300

    inprogress

--- src/components/FrontPageComponents/FrameCheck.tsx
+++
@@ -1,9 +0,0 @@
-import { FaCheck } from 'react-icons/fa6';
-
-export const FstFrameCheck = () => {
-  return (
-    <div className='flex justify-center items-center bg-gray-200 rounded-full w-[26px] h-[26px]'>
-      <FaCheck className='text-emerald-400' />
-    </div>
-  );
-};
