9ded8ef1c5e2466268a3efe0fe74f76fd715fc46
Dzjanis <roah.thaar.g@gmail.com>
2025-09-03 19:06:11 +0300

    in progress

--- src/components/featuresPageComponent/featuresCardsFrame.tsx
+++ src/components/featuresPageComponent/featuresCardsFrame.tsx
@@ -2,12 +2,12 @@ import { FeatureCard } from './FeatureCard';
 import { featuresData } from '../../data/featuresData';
 
 export const FeatureCardsFrame = () => {
-  const cards = featuresData.name.map((_, i) => ({
-    img: featuresData.img[i],
-    name: featuresData.name[i],
-    title: featuresData.title[i],
-    text: featuresData.text[i],
-    listItems: featuresData.listItems,
+  const cards = featuresData.map((feature) => ({
+    img: feature.img,
+    name: feature.name,
+    title: feature.title,
+    text: feature.text,
+    listItems: feature.listItems,
   }));
   return (
     <div className='flex flex-col justify-between items-center w-3/4 py-10 gap-4'>
--- src/data/featuresData.ts
+++ src/data/featuresData.ts
@@ -3,20 +3,33 @@ import featuresImg22 from '../assets/featuresImg22.png';
 import featuresImg23 from '../assets/featuresImg23.png';
 import featuresImg24 from '../assets/featuresImg24.png';
 
-export const featuresData = {
-  img: [featuresImg21, featuresImg22, featuresImg23, featuresImg24],
-  name: ['Transactions', 'Cards', 'Advanced Statistics', 'Saving Accounts'],
-  title: [
-    'Send & receive money instantly',
-    'Manage your cards',
-    'Keep control over your money',
-    'Lorem et ipsum dolor',
-  ],
-  text: [
-    'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
-    'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
-    'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
-    'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
-  ],
-  listItems: ['Malesuada Ipsum', 'Vestibulum', 'Parturient Lorem'],
-};
+export const featuresData = [
+  {
+    img: featuresImg21,
+    name: 'Transactions',
+    title: 'Send & receive money instantly',
+    text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+    listItems: ['Malesuada Ipsum', 'Vestibulum', 'Parturient Lorem'],
+  },
+  {
+    img: featuresImg22,
+    name: 'Cards',
+    title: 'Manage your cards',
+    text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+    listItems: ['Malesuada Ipsum', 'Vestibulum', 'Parturient Lorem'],
+  },
+  {
+    img: featuresImg23,
+    name: 'Advanced Statistics',
+    title: 'Keep control over your money',
+    text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+    listItems: ['Malesuada Ipsum', 'Vestibulum', 'Parturient Lorem'],
+  },
+  {
+    img: featuresImg24,
+    name: 'Saving Accounts',
+    title: 'Lorem et ipsum dolor',
+    text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+    listItems: ['Malesuada Ipsum', 'Vestibulum', 'Parturient Lorem'],
+  },
+];
