97175a5bc7820127abb13d670e5ea923332d2adc
Dzjanis <roah.thaar.g@gmail.com>
2025-09-08 21:05:41 +0300

    compare in progress

---
+++ src/data/compareTablesData.ts
@@ -0,0 +1,71 @@
+export const compareTabelesData = [
+  {
+    title: 'CreditCard',
+    physicalCard: {
+      title: 'Physical Card',
+      icon: 'physical',
+      text: 'Diam in arcu cursus euismod',
+      value: ['optional', 'check', 'check'],
+    },
+    virtualCard: {
+      title: 'Virtual Card',
+      icon: 'virtual',
+      text: 'Diam in arcu cursus euismod',
+      value: ['-', 'up to 2', 'unlimited'],
+    },
+    contactlessPayments: {
+      title: 'Contactless Payments',
+      icon: 'contactless',
+      text: 'Diam in arcu cursus euismod',
+      value: ['check', 'check', 'check'],
+    },
+    mobilePayments: {
+      title: 'Mobile Payments',
+      icon: 'mobile',
+      text: 'Diam in arcu cursus euismod',
+      value: ['check', 'check', 'check'],
+    },
+  },
+  {
+    title: 'Bank Account',
+    freePayments: {
+      title: 'Free Payments Worldwide',
+      icon: 'payments',
+      text: 'Diam in arcu cursus euismod',
+      value: ['-', 'check', 'check'],
+    },
+    freeAtm: {
+      title: 'Free ATM withdrawls',
+      icon: 'atm',
+      text: 'Diam in arcu cursus euismod',
+      value: ['2', '5', '10'],
+    },
+    mobileBanking: {
+      title: 'Mobile Banking',
+      icon: 'banking',
+      text: 'Diam in arcu cursus euismod',
+      value: ['check', 'check', 'check'],
+    },
+  },
+  {
+    title: 'Extra Features',
+    savingAccounts: {
+      title: 'Saving Accounts',
+      icon: 'account',
+      text: 'Diam in arcu cursus euismod',
+      value: ['2', '5', 'unlimited'],
+    },
+    advancedStatistics: {
+      title: 'Advanced Statistics',
+      icon: 'stat',
+      text: 'Diam in arcu cursus euismod',
+      value: ['check', 'check', 'check'],
+    },
+    premiumParnterOffers: {
+      title: 'Premium Parnter Offers',
+      icon: 'partner',
+      text: 'Diam in arcu cursus euismod',
+      value: ['check', 'check', 'check'],
+    },
+  },
+];
--- src/pages/Compare.tsx
+++ src/pages/Compare.tsx
@@ -1,9 +1,11 @@
 import { OneAppFrame } from '../components/commonFrames/OneAppFrame';
 import { SupportSection } from '../components/commonFrames/SupportSection';
+import { CardsCompareFrame } from '../components/compareComponents/CardsCompareFrame';
 
 export const Compare = () => {
   return (
     <div className='flex flex-col justify-between items-center gap-[120px] py-4 w-full bg-white h-auto'>
+      <CardsCompareFrame />
       <OneAppFrame />
       <SupportSection />
     </div>
