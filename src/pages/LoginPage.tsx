f606b3866d4f20b6214a8439e9a589dced98d44d
Dzjanis <Roah.thaar.g@gmail.com>
2025-09-08 16:49:32 +0300

    build completed

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,7 +2,7 @@ import { LoginPageView } from '../components/loginPageComponent/LoginPageView';
 
 export const LoginPage = () => {
   return (
-    <div className='min-h-screen bg-gray-100 flex items-center justify-center'>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <LoginPageView />
     </div>
   );
