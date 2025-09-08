5b32fd36f292223efa009f10f78febc136033c47
Dzjanis <roah.thaar.g@gmail.com>
2025-09-08 21:47:01 +0300

    cardCompareTable in progress

--- src/components/compareComponents/CompareTable.tsx
+++ src/components/compareComponents/CompareTable.tsx
@@ -1,20 +1,22 @@
 type CompareTableProps = {
   title: string;
-  item: [{
-    title: string;
-    icon: string;
-    text: string;
-    value: string[];
-  }];
+  item: [
+    {
+      title: string;
+      icon: string;
+      text: string;
+      value: string[];
+    }
+  ];
 };
 
-export const CompareTable = ({ title, item }: CompareTableProps) => {
+export const CompareTable = ({ title }: CompareTableProps) => {
   return (
     <>
       <div className='flex w-3/4 justify-start items-center border-b'>
         <h2 className='text-xl font-bold'>{title}</h2>
       </div>
-      {item.map(item) => <div className='grid grid-cols-4 w-3/4 h-auto bg-amber-200'></div>}
+      {/* {item.map(item) => <div className='grid grid-cols-4 w-3/4 h-auto bg-amber-200'></div>} */}
     </>
   );
 };
